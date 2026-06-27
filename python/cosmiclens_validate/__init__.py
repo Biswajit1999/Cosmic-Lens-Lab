67fa518e4ac50e81fd4ec05c441588ff5cc75b34
Biswajit Jana <49089568+Biswajit1999@users.noreply.github.com>
2026-06-27 23:33:00 +0100

    feat: expose NFW reference equations

--- python/cosmiclens_validate/__init__.py
+++ python/cosmiclens_validate/__init__.py
@@ -1,6 +1,14 @@
 """Reference validators for CosmicLens Lab."""
 
+from .nfw import nfw_deflection, nfw_potential, nfw_potential_shape
 from .point_mass import point_mass_images, point_mass_magnifications
 from .sis import sis_images
 
-__all__ = ["point_mass_images", "point_mass_magnifications", "sis_images"]
+__all__ = [
+    "nfw_deflection",
+    "nfw_potential",
+    "nfw_potential_shape",
+    "point_mass_images",
+    "point_mass_magnifications",
+    "sis_images",
+]
